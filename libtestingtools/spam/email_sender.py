284f3bf7ddb52063187ed2b9496e836fdb297e33
Your Namelucasfmerino <lucasfmerino@gmail.com>
2022-12-01 22:02:56 -0300

    Manual Mock

--- libtestingtools/spam/email_sender.py
+++ libtestingtools/spam/email_sender.py
@@ -1,11 +1,7 @@
 class Sender:
-    def __init__(self):
-        self.amount_of_emails_sent = 0
-
     def send(self, sender, addressee, subject, body):
         if '@' not in sender:
             raise InvalidMail(f'Invalid sender: {sender}')
-        self.amount_of_emails_sent += 1
         return sender
 
 
--- libtestingtools/tests/test_spam/test_sending_to_users_base.py
+++ libtestingtools/tests/test_spam/test_sending_to_users_base.py
@@ -18,10 +18,39 @@ import pytest
 def test_spam_amount(session, users):
     for user in users:
         session.save_user(user)
-    sender = Sender()
+    sender = SenderMock()
     spam_sender = SpamSender(session, sender)
     spam_sender.send_emails(
         'lucasfmerino@gmail.com',
         'Libtestingtools news #001',
         'Check out the fantastic modules!')
     assert len(users) == sender.amount_of_emails_sent
+
+
+class SenderMock(Sender):
+    def __init__(self):
+        super().__init__()
+        self.amount_of_emails_sent = 0
+        self.shipping_parameters = None
+
+    def send(self, sender, addressee, subject, body):
+        self.shipping_parameters = (sender, addressee, subject, body)
+        self.amount_of_emails_sent += 1
+
+
+def test_spam_parameters(session):
+    user = User(name='Lucas', email='lucasfmerino@gmail.com')
+    session.save_user(user)
+    sender = SenderMock()
+    spam_sender = SpamSender(session, sender)
+    spam_sender.send_emails(
+        'lfmgames@gmail.com',
+        'Libtestingtools news #001',
+        'Check out the fantastic modules!')
+    assert sender.shipping_parameters == (
+        'lfmgames@gmail.com',
+        'lucasfmerino@gmail.com',
+        'Libtestingtools news #001',
+        'Check out the fantastic modules!'
+    )
+
